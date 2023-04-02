afa3f31df119c71c8a618fec784226a106157d9a
Misiorny <krzystan@wp.pl>
2023-04-02 12:12:09 +0200

    tydzien 3 lekcja 5 - grid

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="max-w-md mx-auto w-full ">
+      <nav className="bg-gray-700 text-white px-4 py-2">Nawigacja</nav>
+    </header>
+  );
+};
